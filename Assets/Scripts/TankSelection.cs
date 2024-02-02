dbe99a0fe5f85db2736b2ad12d9bab8d52578b2c
Shobhit Rawat <sbtrwt.se@gmail.com>
2024-02-02 13:36:14 +0530

    Added UI lobby of tank selection

---
+++ Assets/Scripts/TankSelection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+    public TankSpawner tankSpawner;
+   public void BlueTankSelected() {
+        tankSpawner.CreateTank(TankType.BlueTank);
+        Debug.Log("Blue tank selected");
+        this.gameObject.SetActive(false);
+    }
+    public void GreenTankSelected() { 
+        tankSpawner.CreateTank(TankType.GreenTank);
+        this.gameObject.SetActive(false); }
+    public void RedTankSelected() { 
+        tankSpawner.CreateTank(TankType.RedTank); 
+        this.gameObject.SetActive(false); }
+}
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -19,12 +19,27 @@ public class TankSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateTank();
+        //CreateTank();
     }
 
-    private void CreateTank()
+    public void CreateTank(TankType tankType)
     {
         TankModel tank = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankType, tankList[0].color);
+        switch (tankType)
+        {
+            case TankType.BlueTank:
+                tank = new TankModel(tankList[1].movementSpeed, tankList[1].rotationSpeed, tankList[1].tankType, tankList[1].color);
+                break;
+            case TankType.GreenTank:
+                tank = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankType, tankList[0].color);
+                break;
+            case TankType.RedTank:
+                tank = new TankModel(tankList[2].movementSpeed, tankList[2].rotationSpeed, tankList[2].tankType, tankList[2].color);
+                break;
+            default:
+                tank = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankType, tankList[0].color);
+                break;
+        }
         TankController tankController = new TankController(tank, tankView);
     }
   
